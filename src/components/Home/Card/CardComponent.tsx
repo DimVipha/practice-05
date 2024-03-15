8214481fa9c66ecabf7165fdd58af4ea030f0f62
vipha <annaella976@gmail.com>
2024-03-15 16:03:32 +0700

    pratice-005

--- src/components/Home/Card/CardComponent.tsx
+++ src/components/Home/Card/CardComponent.tsx
@@ -1,19 +1,20 @@
-import { Card } from 'flowbite-react';
-import { Products } from '../HomeComponent';
+import { Card } from "flowbite-react";
+import { Products } from "../HomeComponent";
 
-export default function CardComponent({title,description,image}:Products) {
+export default function CardComponent({ title, description, image }: Products) {
   return (
     <Card
-    className="max-w-sm"
-    renderImage={() => <img width={300} height={300} src={image} alt="image 1" />}
-  >
-    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-      {title}
-    </h5>
-    <p className="font-normal line-clamp-3 text-gray-700 dark:text-gray-400">
-      {description}
-    </p>
-  </Card>
-    
+      className="max-w-sm"
+      renderImage={() => (
+        <img width={300} height={300} src={image} alt="image 1" />
+      )}
+    >
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {title}
+      </h5>
+      <p className="font-normal line-clamp-3 text-gray-700 dark:text-gray-400">
+        {description}
+      </p>
+    </Card>
   );
 }
